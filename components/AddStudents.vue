74f3999910a1c6a24a7545e6d8b3b37d8e9db487
Abdou <2000abdallah2014@gmail.com>
2023-06-24 12:49:50 +0100

    prevent user from entering empty rows in names f

--- components/AddStudents.vue
+++ components/AddStudents.vue
@@ -1,10 +1,11 @@
 <template >
-    <v-sheet width="1000" class="mx-auto">
+    <v-sheet width="1000" class="mx-auto mb-10">
         <h1 class="text-center text-3xl my-2 mb-10 text-blue-500">Set up your table</h1>
         Here you can set up a able of your attendents, you can add a criteria that can help you to sort...(ie: a teacher can
-        use marks as an additional criteria)
-        <v-table>
-            <thead>
+        use marks as an additional criteria). <br>
+        click add data after you complete entering data in the text field
+        <v-table class="my-4">
+            <thead class="bg-gray-800">
                 <tr>
                     <th class="text-left">Names</th>
                     <th class="text-left"><v-text-field maxlength="10" :counter="10"
@@ -19,15 +20,21 @@
                 </tr>
                 <tr>
                     <td>
-                        <v-textarea label="enter new students' list" v-model="newStudents" rows="2"></v-textarea>
+                        <v-textarea label="enter new students' list" v-model="newStudents" rows="2"
+                            :rules="namesRule"></v-textarea>
                     </td>
                     <td>
                         <v-textarea label="enter new data's list" v-model="newMarks" rows="2"></v-textarea>
                     </td>
                 </tr>
+
             </tbody>
+
         </v-table>
-        <v-btn @click="addNewData">Add data</v-btn>
+        <div class="flex justify-center ">
+            <v-btn class="mx-4" @click="addNewData" variant="tonal" color="light-green-lighten-4">Add data</v-btn>
+            <v-btn class="mx-4" @click="clearTable" variant="tonal" color="red-lighten-4">clear</v-btn>
+        </div>
     </v-sheet>
 </template>
 
@@ -52,13 +59,20 @@ let newMarks = ref('')
 const addNewData = () => {
     const newStudentsArray = newStudents.value.split('\n')
     const newMarksArray = newMarks.value.split('\n')
+
     newStudentsArray.forEach((el, i) => {
-        namesTable.value.tableData.push({
-            name: el,
-            fieldOne: newMarksArray[i],
-        })
+        if (el) {
+            namesTable.value.tableData.push({
+                name: el,
+                fieldOne: newMarksArray[i],
+            })
+        }
     })
     newStudents.value = newMarks.value = "";
+}
+
+const clearTable = () => {
+    namesTable.value.tableData = []
 }
 
 
@@ -83,6 +97,16 @@ const fieldOneTitleRule = [
         if (value && namesTable.value.criteriaOneTitle || !value) return true;
         return "Enter a criteria name"
     }
+]
+
+
+const namesRule = [
+    value => {
+        if (value.match(/\n\n/)) return "you can't have empty row(s)"
+
+        return true
+    },
+
 ]
 
 </script>
