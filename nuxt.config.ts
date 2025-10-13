435ac604431603150e1337d791161ab315048ca2
Abdou <2000abdallah2014@gmail.com>
2025-10-13 21:37:19 +0100

    config of i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['vuetify/lib/styles/main.sass','@mdi/font/css/materialdesignicons.min.css',],
-    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','@nuxtjs/tailwindcss','@vueuse/nuxt',],
-    pinia: {
-      autoImports: [
-        // automatically imports `defineStore`
-        'defineStore', // import { defineStore } from 'pinia'
-        ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-      ],
-    },
-    devtools: { enabled: false },
-      build: {
-    transpile: ['vuetify'],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/i18n"
+  ],
+  i18n: {
+    defaultLocale: "en",
+    strategy: "no_prefix", // no /en or /ar in the URL
+    locales: [
+      { code: "en", name: "English", dir: "ltr", },
+      { code: "ar", name: "العربية", dir: "rtl", },
+    ],
+    detectBrowserLanguage: false, // we'll handle ?lang manually
+  },
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+  devtools: { enabled: false },
+  build: {
+    transpile: ["vuetify"],
   },
   vite: {
     define: {
-      'process.env.DEBUG': false,
+      "process.env.DEBUG": false,
     },
   },
-})
+});
