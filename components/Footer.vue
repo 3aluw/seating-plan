f0fa927d3556dcb3aef0ee8879b80ecdc1511d9a
Abdou <2000abdallah2014@gmail.com>
2025-10-16 19:10:41 +0100

    changed footer style

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="cont bg-black text-white text-center p-5 mt-8">
+    <div class="cont border-t-2 border-slate-300 bg-stone-100 text-center p-5 mt-8">
         <i18n-t class="pt-5" keypath="footer.builtBy" tag="p">
-            <a href="https://portfolio-3aluw.vercel.app/" rel="noopener noreferrer" class="underline text-blue-300">{{
+            <a href="https://portfolio-3aluw.vercel.app/" rel="noopener" target="_blank" class="underline text-blue-600">{{
                 $t("footer.dev") }}</a>
         </i18n-t>
     </div>
